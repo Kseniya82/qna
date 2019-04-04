b4322fd9f4005da6b6d597d4c8320b961ce9e04e
Kseniya82 <chirkovaksenia82@gmail.com>
2019-04-04 09:43:10 +0300

    Add action create to AnswersController with tests

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -3,9 +3,18 @@ class AnswersController < ApplicationController
     @answer = question.answers.new
   end
 
+  def create
+    @answer = question.answers.new(answers_params)
+    render :new unless @answer.save
+  end
+
   private
 
   def question
     @question ||= Question.find(params[:question_id])
+  end
+
+  def answers_params
+    params.require(:answer).permit(:question_id, :body)
   end
 end
--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -16,4 +16,26 @@ RSpec.describe AnswersController, type: :controller do
       expect(response).to render_template :new
     end
   end
+
+  describe 'POST #create' do
+    context 'with valid attributes' do
+      it 'save a new answer in database' do
+        expect { post :create, params: { question_id: question, answer: attributes_for(:answer) } }
+        .to change(question.answers, :count).by(1)
+      end
+    end
+
+    context 'with invalid attributes' do
+      it ' not save a new answer in database' do
+        expect { post :create, params: { question_id: question, answer: attributes_for(:answer, :invalid) } }
+        .to_not change(Answer, :count)
+      end
+
+      it 're-render new view' do
+        post :create, params: { question_id: question, answer: attributes_for(:answer, :invalid) }
+        expect(response).to render_template :new
+      end
+    end
+  end
+
 end
