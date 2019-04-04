2dc332494afb114474b659ddba1a2cde94307675
Kseniya82 <chirkovaksenia82@gmail.com>
2019-04-04 08:28:38 +0300

    Add action create in QuestionController with tests

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -14,5 +14,24 @@ RSpec.describe QuestionsController, type: :controller do
     end
   end
 
+  describe 'POST #create' do
 
+    context 'with valid attributes' do
+      it 'save a new question in database' do
+        expect { post :create, params: { question: attributes_for(:question) } }.to change(Question, :count).by(1)
+      end
+    end
+
+    context 'with invalid attributes' do
+      it ' not save question in database' do
+        expect { post :create, params: { question: attributes_for(:question, :invalid) } }.to_not change(Question, :count)
+      end
+
+      it 're-render new view' do
+        post :create, params: { question: attributes_for(:question, :invalid) }
+        expect(response).to render_template :new
+      end
+
+    end
+  end
 end
