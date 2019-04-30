c27d81373e24d8ab8840277e3e4afa13b3343e83
Kseniya82 <chirkovaksenia82@gmail.com>
2019-04-30 21:05:07 +0300

    Only author may edit own answer

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -51,14 +51,52 @@ RSpec.describe AnswersController, type: :controller do
       end
     end
 
-    context ' Not author tried delete question' do
-      it 'deletes the question' do
+    context ' Not author tried delete answer' do
+      it 'no deletes the answer' do
         expect { delete :destroy, params: { id: answer } }.to_not change(Answer, :count)
       end
 
-      it 'redirects to index' do
+      it 'redirects to question show' do
         delete :destroy, params: { id: answer }
         expect(response).to redirect_to question_path(answer.question)
+      end
+    end
+  end
+
+  describe 'PATCH #update' do
+    let!(:answer) { create(:answer, question: question) }
+    let!(:own_answer) { create(:answer, question: question, user: user) }
+
+    context 'with valid attributes' do
+      it 'changes answer attributes' do
+        patch :update, params: { id: own_answer, answer: { body: 'new body' } }, format: :js
+        own_answer.reload
+        expect(own_answer.body).to eq 'new body'
+      end
+
+      it 'renders update view' do
+        patch :update, params: { id: own_answer, answer: { body: 'new body' } }, format: :js
+        expect(response).to render_template :update
+      end
+    end
+
+    context 'with invalid attributes' do
+      it 'does not change answer attributes' do
+        expect do
+          patch :update, params: { id: own_answer, answer: attributes_for(:answer, :invalid) }, format: :js
+        end.to_not change(own_answer, :body)
+      end
+
+      it 'renders update view' do
+        patch :update, params: { id: own_answer, answer: attributes_for(:answer, :invalid) }, format: :js
+        expect(response).to render_template :update
+      end
+    end
+    context 'Not author tried update answer' do
+      it 'does not change answer attributes' do
+        expect do
+          patch :update, params: { id: answer, answer: attributes_for(:answer, :invalid) }, format: :js
+        end.to_not change(answer, :body)
       end
     end
   end
