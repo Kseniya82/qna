869a146c6da963443642c112ec7401ed9edd357b
Kseniya82 <chirkovaksenia82@gmail.com>
2019-05-14 22:29:05 +0300

    Author question may mark answer as best, this answer may be only one

---
+++ spec/features/answer/best_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+feature 'Author question can mark best one answer for own question', %q{
+  In order to choose the best answer
+  As an author of question
+  I'd like ot be able to mark answer as best
+} do
+
+  given!(:user) { create(:user) }
+  given!(:question) { create(:question) }
+  given!(:own_question) { create(:question, user: user) }
+  given!(:answers) { create_list(:answer, 2, question: own_question) }
+
+  scenario 'Unauthenticated can not mark answer as best' do
+    visit question_path(question)
+
+    expect(page).to_not have_link 'Mark as best'
+  end
+
+  context 'Authentication user', js: true do
+    background do
+      sign_in(user)
+    end
+
+    scenario 'Author of question can mark answer as best' do
+      visit question_path(own_question)
+
+      first('.mark-as-best').click
+      first('.mark-as-best').click
+
+      expect(page).to have_css('.best', count: 1)
+    end
+
+    scenario 'Author of question can mark other answer as best if question alredy have best answer' do
+      answers.first.best = true
+      visit question_path(own_question)
+      first('.mark-as-best').click
+
+      expect(page).to have_css('.best', count: 1)
+    end
+
+    scenario 'Not author can not mark answer as best' do
+      visit question_path(question)
+
+      expect(page).to_not have_link 'Mark as best'
+    end
+  end
+end
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -5,4 +5,25 @@ RSpec.describe Answer, type: :model do
   it { should belong_to :user }
 
   it { should validate_presence_of :body }
+
+  let!(:question) { create(:question) }
+  let!(:answer) { create(:answer, question: question) }
+  let!(:answer2) { create(:answer, question: question) }
+
+  it 'mark answer as best' do
+    answer.best!
+    expect(answer).to be_best
+  end
+
+  it 'Best answer may be only one' do
+    answer.best!
+    answer2.best!
+    answer.reload
+    expect(answer).to_not be_best
+  end
+
+  it 'Best answer on the first place' do
+    answer2.best!
+    expect(Answer.first).to eq(answer2)
+  end
 end
