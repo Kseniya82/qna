7a18f1045be1bdcd6b8041827764327c265a308f
Kseniya82 <chirkovaksenia82@gmail.com>
2019-05-23 17:14:14 +0300

    User may add files to answer during edit

--- spec/features/answer/edit_spec.rb
+++ spec/features/answer/edit_spec.rb
@@ -54,5 +54,17 @@ feature 'User can edit his answer', %q{
         expect(page).to_not have_link 'Edit'
       end
     end
+
+    scenario 'edits a answer with attached files' do
+      within '.answers' do
+        click_on 'Edit'
+        fill_in 'Your answer', with: 'edited answer'
+        attach_file 'File', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
+        click_on 'Save'
+
+        expect(page).to have_link 'rails_helper.rb'
+        expect(page).to have_link 'spec_helper.rb'
+      end
+    end
   end
 end
