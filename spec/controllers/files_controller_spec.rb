83ae7d7fab2c5d475869a51e1f8fc12490a6aa2e
Kseniya82 <chirkovaksenia82@gmail.com>
2019-05-28 11:01:18 +0300

    Change files controller tests

--- spec/controllers/files_controller_spec.rb
+++ spec/controllers/files_controller_spec.rb
@@ -8,61 +8,29 @@ before { login(user) }
   describe 'DELETE #destroy' do
     let!(:file) { fixture_file_upload("#{Rails.root}/spec/rails_helper.rb") }
 
-    context 'answer' do
-      context 'author tried delete file' do
-        let(:own_answer) { create(:answer, user: user, files: file) }
-        it 'deletes the file from  answers files' do
-          expect { delete :destroy, params: { id: own_answer.files.first}, format: :js }
+    context 'author tried delete file' do
+      let(:own_answer) { create(:answer, user: user, files: file) }
+      it 'deletes the file from  answers files' do
+        expect { delete :destroy, params: { id: own_answer.files.first}, format: :js }
           .to change(own_answer.files, :count).by(-1)
-        end
-
-        it 'renders destroy view' do
-          delete :destroy, params:  { id: own_answer.files.first }, format: :js
-          expect(response).to render_template :destroy
-        end
       end
 
-
-      context ' Not author tried delete file' do
-        let(:answer) { create(:answer, files: file) }
-        it 'no deletes the file' do
-          expect { delete :destroy, params: { id: answer.files.first }, format: :js }
-          .to_not change(answer.files, :count)
-        end
-
-        it 'redirects to answer show' do
-          delete :destroy, params: { id: answer.files.first }, format: :js
-          expect(response).to redirect_to root_path
-        end
+      it 'renders destroy view' do
+        delete :destroy, params:  { id: own_answer.files.first }, format: :js
+        expect(response).to render_template :destroy
       end
     end
 
-    context 'answer' do
-      context 'author tried delete file' do
-        let(:own_answer) { create(:answer, user: user, files: file) }
-        it 'deletes the file from  answers files' do
-          expect { delete :destroy, params: { id: own_answer.files.first}, format: :js }
-          .to change(own_answer.files, :count).by(-1)
-        end
-
-        it 'renders destroy view' do
-          delete :destroy, params:  { id: own_answer.files.first }, format: :js
-          expect(response).to render_template :destroy
-        end
+    context ' Not author tried delete file' do
+      let(:answer) { create(:answer, files: file) }
+      it 'no deletes the file' do
+        expect { delete :destroy, params: { id: answer.files.first }, format: :js }
+          .to_not change(answer.files, :count)
       end
 
-
-      context ' Not author tried delete file' do
-        let(:answer) { create(:answer, files: file) }
-        it 'no deletes the file' do
-          expect { delete :destroy, params: { id: answer.files.first }, format: :js }
-          .to_not change(answer.files, :count)
-        end
-
-        it 'redirects to root path' do
-          delete :destroy, params: { id: answer.files.first }, format: :js
-          expect(response).to redirect_to root_path
-        end
+      it 'redirects to root_path' do
+        delete :destroy, params: { id: answer.files.first }, format: :js
+        expect(response).to redirect_to root_path
       end
     end
   end
