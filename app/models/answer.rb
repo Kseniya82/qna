c831a5e8476fbbf54880d4f7cad919d2a991d7ee
Kseniya82 <chirkovaksenia82@gmail.com>
2019-06-24 18:15:33 +0300

    User can add many links to question and answer

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,10 +1,13 @@
 class Answer < ApplicationRecord
   default_scope { order(best: :desc) }
 
+  has_many :links, dependent: :destroy, as: :linkable
   belongs_to :question
   belongs_to :user
 
   has_many_attached :files
+
+  accepts_nested_attributes_for :links, reject_if: :all_blank
 
   validates :body, presence: true
 
